d1941aa44039fe9675c4d5f5a28daf9afe0502f5
Travis Ralston <travpc@gmail.com>
2018-10-19 20:00:25 -0600

    Move nl2br to html layer

--- src/processing/layers/message/html.js
+++ src/processing/layers/message/html.js
@@ -3,7 +3,7 @@ const striptags = require("striptags");
 module.exports = (webhook, matrix) => {
     if (webhook.format === "html") {
         matrix.event.format = "org.matrix.custom.html";
-        matrix.event.formatted_body = matrix.event.body;
+        matrix.event.formatted_body = matrix.event.body.replace("\n", "<br />");
         matrix.event.body = striptags(matrix.event.formatted_body);
     }
 };
--- src/processing/layers/message/slack_links.js
+++ src/processing/layers/message/slack_links.js
@@ -2,22 +2,19 @@ module.exports = (webhook, matrix) => {
     // Reference: https://api.slack.com/docs/message-formatting#linking_to_urls
     // Slack also accepts e.g. <example.com|DISPLAY_STR> but that results in a relative path
 
-    // Match <PROTOCOL://REST_OF_URL|DISPLAY_STR>
-    const linkRegex = /<([a-zA-Z]+):\/\/([^|>]+?)\|([^|>]+?)>/g;
-    // Match <PROTOCOL://REST_OF_URL>
-    const linkRegex2 = /<([a-zA-Z]+):\/\/([^|>]+?)>/g;
-    // Match <mailto:ADDRESS|DISPLAY_STR>
-    const mailtoRegex = /<mailto:([^|>]+?)\|([^|>]+?)>/g;
-    // Match <mailto:ADDRESS>
-    const mailtoRegex2 = /<mailto:([^|>]+?)>/g;
+    const linkRegex = /<([a-zA-Z]+):\/\/([^|>]+?)\|([^|>]+?)>/g; // Match <PROTOCOL://REST_OF_URL|DISPLAY_STR>
+    const linkRegex2 = /<([a-zA-Z]+):\/\/([^|>]+?)>/g; // Match <PROTOCOL://REST_OF_URL>
+    const mailtoRegex = /<mailto:([^|>]+?)\|([^|>]+?)>/g; // Match <mailto:ADDRESS|DISPLAY_STR>
+    const mailtoRegex2 = /<mailto:([^|>]+?)>/g; // Match <mailto:ADDRESS>
 
     // Apply regex'es
+    const before = matrix.event.body;
     matrix.event.body = matrix.event.body.replace(linkRegex, "<a href='$1://$2'>$3</a>");
     matrix.event.body = matrix.event.body.replace(linkRegex2, "<a href='$1://$2'>$1://$2</a>");
     matrix.event.body = matrix.event.body.replace(mailtoRegex, "<a href='mailto:$1'>$2</a>");
     matrix.event.body = matrix.event.body.replace(mailtoRegex2, "<a href='mailto:$1'>mailto:$1</a>");
 
-    // TODO: Put this somewhere else
-    // Handle Slack Multiline Messages
-    matrix.event.body = matrix.event.body.replace('\n', '<br>');
+    if (before !== matrix.event.body) {
+        webhook.format = "html"; // to force the HTML layer to process it
+    }
 };
