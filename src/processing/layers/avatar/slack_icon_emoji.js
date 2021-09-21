d410b468827fbb026d06e351e01f34893b8eb280
Stefan Warnat <ich@stefanwarnat.de>
2021-09-21 23:15:43 +0200

    Update slack_icon_emoji.js
    Fix cheerio usage

--- src/processing/layers/avatar/slack_icon_emoji.js
+++ src/processing/layers/avatar/slack_icon_emoji.js
@@ -6,11 +6,11 @@ emojione.emojiSize = '128';
 module.exports = (webhook, matrix) => {
     if (!matrix.sender.avatarUrl && webhook.icon_emoji) {
         // HACK: We really shouldn't have to do this element -> url conversion
-
         const imgElement = emojione.shortnameToImage(webhook.icon_emoji);
         if (imgElement == webhook.icon_emoji) return;
 
-        const srcUrl = cheerio.load(imgElement).attr('src');
-        matrix.sender.avatarUrl = srcUrl;
+        const $ = cheerio.load(imgElement);
+
+        matrix.sender.avatar_url = $('img').attr('src');
     }
 };
