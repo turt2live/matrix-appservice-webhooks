1644c72295c866f6ccd895e12e35d5b1e7e3521b
Stefan Warnat <ich@stefanwarnat.de>
2021-09-21 23:16:08 +0200

    Update slack_icon_emoji.js

--- src/processing/layers/avatar/slack_icon_emoji.js
+++ src/processing/layers/avatar/slack_icon_emoji.js
@@ -4,7 +4,7 @@ const cheerio = require("cheerio");
 emojione.emojiSize = '128';
 
 module.exports = (webhook, matrix) => {
-    if (!matrix.sender.avatarUrl && webhook.icon_emoji) {
+    if (!matrix.sender.avatar_url && webhook.icon_emoji) {
         // HACK: We really shouldn't have to do this element -> url conversion
         const imgElement = emojione.shortnameToImage(webhook.icon_emoji);
         if (imgElement == webhook.icon_emoji) return;
